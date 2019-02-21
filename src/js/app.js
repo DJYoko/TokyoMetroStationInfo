65e2925577202295c8936ddfeca577bf78cd6bcb
dj_yoko <yokochan.recurrent@gmail.com>
2019-02-22 00:11:41 +0900

    remove initial (tutorial) demo coponents. summarize root component.

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {MyComponent} from './MyComponent';
+import { LineContainer } from './LineContainer';
+  
+const LINE_CODES = [
+	'l11302',
+	'l28001',
+	'l28002',
+	'l28003',
+	'l28004',
+	'l28005',
+	'l28006',
+	'l28008',
+	'l28009',
+	'l28010',
+];
+
+export class Container extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+				<LineContainer line_codes={LINE_CODES} />
+			</div>
+   		 );
+    }
+}
 
 ReactDOM.render(
-  <MyComponent />,
+  <Container />,
   document.getElementById('app')
 );
