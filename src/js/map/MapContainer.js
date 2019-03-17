823ad0eef7b7fbc2e001120e4232ec55bef21319
dj_yoko <yokochan.recurrent@gmail.com>
2019-03-17 22:41:30 +0900

    set bound setting  after Line selected

--- src/js/map/MapContainer.js
+++ src/js/map/MapContainer.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import PropTypes from "prop-types";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-import _JSXStyle from "styled-jsx/style";
-import css from "styled-jsx/css";
 
 export class MapContainer extends Component {
   constructor(props) {
@@ -11,10 +9,13 @@ export class MapContainer extends Component {
   }
 
   render() {
-    const _position = this.getInitialMapPosition();
-
     return (
-      <Map center={_position} zoom={14} style={this.mayMapStyle()}>
+      <Map
+        center={this.props.position}
+        zoom="14"
+        style={this.mayMapStyle()}
+        ref="map"
+      >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -22,6 +23,14 @@ export class MapContainer extends Component {
         {this.stationMarkers(this.props.line.station_l)}
       </Map>
     );
+  }
+
+  componentDidMount() {
+    this.setMapBound();
+  }
+
+  componentDidUpdate() {
+    this.setMapBound();
   }
 
   mayMapStyle() {
@@ -34,28 +43,44 @@ export class MapContainer extends Component {
       </Marker>
     ));
   }
+  setMapBound() {
+    if (!this.hasStations) {
+      // default, no station data added.
+      return false;
+    }
+    const stations = this.props.line.station_l;
+    const _bound = {
+      top: stations[0].lon,
+      left: stations[0].lat,
+      bottom: stations[0].lon,
+      right: stations[0].lat
+    };
 
-  getInitialMapPosition() {
+    stations.forEach((station, index) => {
+      _bound.right = Math.max(_bound.right, station.lat);
+      console.log(station.lat, station.lon);
+      _bound.left = Math.min(_bound.left, station.lat);
+      _bound.top = Math.max(_bound.top, station.lon);
+      _bound.bottom = Math.min(_bound.bottom, station.lon);
+    });
+    console.log(_bound);
+    this.refs.map.leafletElement.fitBounds(
+      [[_bound.left, _bound.bottom], [_bound.right, _bound.top]],
+      {
+        padding: [20, 20]
+      }
+    );
+  }
+  hasStations() {
     if (
       typeof this.props.line !== "object" ||
       typeof this.props.line.station_l !== "object" ||
       this.props.line.station_l.length === 0
     ) {
-	  // defaut positions
-      return this.props.position;
+      // default
+      return false;
     }
-
-    const stations = this.props.line.station_l;
-    let sumLat = 0;
-    let sumLon = 0;
-
-    stations.forEach((station, index) => {
-      sumLat = sumLat + station.lat;
-      sumLon = sumLon + station.lon;
-    });
-    const avgLat = sumLat / stations.length;
-    const avgLon = sumLon / stations.length;
-    return [avgLat, avgLon];
+    return true;
   }
 }
 
