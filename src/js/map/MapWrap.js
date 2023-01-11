ef4cd6a78335f657783dd5455a8544c90bb9354b
DJYoko <yokochan.recurrent@gmail.com>
2023-01-11 22:47:10 +0900

    set init positions

--- src/js/map/MapWrap.js
+++ src/js/map/MapWrap.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
 import PropTypes from 'prop-types';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+
+const INIT_CENTER = {
+  lat: 35.658034,
+  lng: 139.701636,
+};
 
 export class MapWrap extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      mapBound: [
+        {
+          lng: INIT_CENTER.lng - 0.1,
+          lat: INIT_CENTER.lat - 0.1,
+        },
+        { lng: INIT_CENTER.lng + 0.1, lat: INIT_CENTER.lat + 0.1 },
+      ],
+    };
   }
+  // , 139.701636
 
   render() {
     return (
-      <MapContainer center={this.props.position} zoom='14' style={this.mayMapStyle()} ref='mapRef'>
+      <MapContainer bounds={this.state.mapBound} style={this.mayMapStyle()}>
         <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
         {this.stationMarkers(this.props.line.station_l)}
       </MapContainer>
@@ -55,15 +70,10 @@ export class MapWrap extends Component {
       _bound.bottom = Math.min(_bound.bottom, station.lon);
     });
 
-    // this.mapRef.leafletElement.fitBounds(
-    //   [
-    //     [_bound.left, _bound.bottom],
-    //     [_bound.right, _bound.top],
-    //   ],
-    //   {
-    //     padding: [20, 20],
-    //   }
-    // );
+    this.state.mapBound[0].lat = _bound.left;
+    this.state.mapBound[1].lat = _bound.right;
+    this.state.mapBound[0].lat = _bound.left;
+    this.state.mapBound[1].lat = _bound.right;
   }
   hasStations() {
     if (typeof this.props.line !== 'object' || typeof this.props.line.station_l !== 'object' || this.props.line.station_l.length === 0) {
